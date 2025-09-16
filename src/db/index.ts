134eb4de094cb25f7cc939074c1875f1bbedcf6a
gautamk01 <gautamkrishna.mooppil@gmail.com>
2025-09-16 13:49:32 +0530

    Bug Fix : A small Type error with the Schema

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,11 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not set");
+}
+
+const sql = neon(process.env.DATABASE_URL);
 const db = drizzle(sql, { schema });
 
 export default db;
